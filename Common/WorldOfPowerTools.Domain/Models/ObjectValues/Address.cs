cd68e7790375699305fba329679c96f036d21ea6
Nike073i <nike073i@mail.ru>
2022-11-09 23:24:56 +0400

    Реализация Address и его тестов

--- Common/WorldOfPowerTools.Domain/Models/ObjectValues/Address.cs
+++ Common/WorldOfPowerTools.Domain/Models/ObjectValues/Address.cs
@@ -14,7 +14,45 @@ namespace WorldOfPowerTools.Domain.Models.ObjectValues
 
         public Address(string country, string city, string street, string house, int flat, string postalCode)
         {
-            throw new Exception("Not implemented");
+            if (string.IsNullOrEmpty(country)) throw new ArgumentNullException(nameof(country));
+            if (string.IsNullOrEmpty(city)) throw new ArgumentNullException(nameof(city));
+            if (string.IsNullOrEmpty(street)) throw new ArgumentNullException(nameof(street));
+            if (string.IsNullOrEmpty(house)) throw new ArgumentNullException(nameof(house));
+            if (string.IsNullOrEmpty(postalCode)) throw new ArgumentNullException(nameof(postalCode));
+            if (flat < 1 || flat > 9999) throw new ArgumentOutOfRangeException(nameof(flat));
+
+            Country = country;
+            City = city;
+            Street = street;
+            House = house;
+            Flat = flat;
+            PostalCode = postalCode;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is Address other) && Equals(other);
+        }
+
+        public bool Equals(Address other)
+        {
+            if (other == null) return false;
+            return Country == other.Country &&
+                City == other.City &&
+                Street == other.Street &&
+                House == other.House &&
+                Flat == other.Flat &&
+                PostalCode == other.PostalCode;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Country, City, Street, House, Flat, PostalCode);
+        }
+
+        public override string ToString()
+        {
+            return $"{Country}, г.{City}, ул.{Street}, д.{House}, кв.{Flat}, индекс {PostalCode}";
         }
     }
 }
---
+++ Tests/Common/WorldOfPowerTools.Domain.Test/Models/ObjectValues/AddressTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System;
+using WorldOfPowerTools.Domain.Models.ObjectValues;
+
+namespace WorldOfPowerTools.Domain.Test.Models.ObjectValues
+{
+    public class AddressTest
+    {
+        [Test]
+        [TestCaseSource(nameof(IncorrectConstructCases))]
+        public void CreateAddressIncorrect(string country, string city, string street, string house, int flat, string postalCode, Type awaitingException)
+        {
+            TestDelegate construct = () => new Address(country, city, street, house, flat, postalCode);
+            Assert.Throws(awaitingException, construct);
+        }
+
+        [Test]
+        public void CreateAddressCorrect()
+        {
+            var expAddress = new Address("Тестовая страна", "Тестовый город", "Тестовая улица", "30б", 100, "443444");
+            var address = CreateAddress();
+            Assert.AreEqual(expAddress, address);
+        }
+
+        public Address CreateAddress()
+        {
+            string country = "Тестовая страна";
+            string city = "Тестовый город";
+            string street = "Тестовая улица";
+            string house = "30б";
+            int flat = 100;
+            string postalCode = "443444";
+            return new Address(country, city, street, house, flat, postalCode);
+        }
+
+        static object[] IncorrectConstructCases =
+        {
+            new object?[] { "", "Тестовый город", "Тестовая улица", "30б", 100, "443444", typeof(ArgumentNullException) },
+            new object?[] { null, "Тестовый город", "Тестовая улица", "30б", 100, "443444", typeof(ArgumentNullException) },
+            new object?[] { "Тестовая страна", "", "Тестовая улица", "30б", 100, "443444", typeof(ArgumentNullException) },
+            new object?[] { "Тестовая страна", null, "Тестовая улица", "30б", 100, "443444", typeof(ArgumentNullException) },
+            new object?[] { "Тестовая страна", "Тестовый город", "", "30б", 100, "443444", typeof(ArgumentNullException) },
+            new object?[] { "Тестовая страна", "Тестовый город", null, "30б", 100, "443444", typeof(ArgumentNullException) },
+            new object?[] { "Тестовая страна", "Тестовый город", "Тестовая улица", "", 100, "443444", typeof(ArgumentNullException) },
+            new object?[] { "Тестовая страна", "Тестовый город", "Тестовая улица", null, 100, "443444", typeof(ArgumentNullException) },
+            new object?[] { "Тестовая страна", "Тестовый город", "Тестовая улица", "30б", 0, "443444", typeof(ArgumentOutOfRangeException) },
+            new object?[] { "Тестовая страна", "Тестовый город", "Тестовая улица", "30б", 10000, "443444", typeof(ArgumentOutOfRangeException) },
+            new object?[] { "Тестовая страна", "Тестовый город", "Тестовая улица", "30б", 100, "", typeof(ArgumentNullException) },
+            new object?[] { "Тестовая страна", "Тестовый город", "Тестовая улица", "30б", 100, null, typeof(ArgumentNullException) },
+    };
+    }
+}
