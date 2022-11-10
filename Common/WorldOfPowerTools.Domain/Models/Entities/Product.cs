b689be905bd2c4363edb627294ef8476c77c99d9
Nike073i <nike073i@mail.ru>
2022-11-11 00:20:15 +0400

    Тесты и реализация товара

--- Common/WorldOfPowerTools.Domain/Models/Entities/Product.cs
+++ Common/WorldOfPowerTools.Domain/Models/Entities/Product.cs
@@ -4,27 +4,120 @@ namespace WorldOfPowerTools.Domain.Models.Entities
 {
     public class Product : Entity
     {
-        public string Name { get; set; }
-        public double Price { get; set; }
+        public static readonly int MinAvailableQuantity = 1;
+        public static readonly int MinNameLength = 5;
+        public static readonly int MaxNameLength = 100;
+        public static readonly int MaxDescriptionLength = 2000;
+        public static readonly int MinPrice = 0;
+        public static readonly int MaxPrice = 999_999_999;
+        public static readonly int MinQuantity = 0;
+        public static readonly int MaxQuantity = 9999;
+
+        private static readonly string RemoveMoreFromStoreThanExistsErrorMessage = "В наличии нет указанного количества товара";
+        private static readonly string AddMoreToStoreThanMaxQuantityErrorMessage = "Количество товара при добавлении превышает ограничение максимального количества";
+
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                ThrowExceptionIfNameIncorrect(value);
+                _name = value;
+            }
+        }
+
+        private double _price;
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                ThrowExceptionIfPriceIncorrect(value);
+                _price = value;
+            }
+        }
+
         public Category Category { get; set; }
-        public string Description { get; set; }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                ThrowExceptionIfDescriptionIncorrect(value);
+                _description = value;
+            }
+        }
+
         public int Quantity { get; protected set; }
-        public bool Availablity { get; protected set; }
+        public bool Availability { get; protected set; }
 
 #nullable disable
         protected Product() { }
 
         public Product(string name, double price, Category category, string description, int quantity, bool availability = true)
         {
-            throw new Exception("Not implemented");
+            ThrowExceptionIfNameIncorrect(name);
+            ThrowExceptionIfPriceIncorrect(price);
+            ThrowExceptionIfDescriptionIncorrect(description);
+            ThrowExceptionIfQuantityIncorrect(quantity);
+
+            Name = name;
+            Price = price;
+            Category = category;
+            Description = description;
+            Quantity = quantity;
+            Availability = availability;
         }
+
         public Product AddToStore(int quantity)
         {
-            throw new Exception("Not implemented");
+            if (quantity < 0) throw new ArgumentOutOfRangeException(nameof(quantity));
+            int newQuantity = Quantity + quantity;
+            if (newQuantity > MaxQuantity) throw new InvalidOperationException(AddMoreToStoreThanMaxQuantityErrorMessage);
+            Quantity = newQuantity;
+            CheckAndSetAvailability(newQuantity);
+            return this;
         }
+
         public Product RemoveFromStore(int quantity)
         {
-            throw new Exception("Not implemented");
+            if (quantity < 0) throw new ArgumentOutOfRangeException(nameof(quantity));
+            int newQuantity = Quantity - quantity;
+            if (newQuantity < MinQuantity) throw new InvalidOperationException(RemoveMoreFromStoreThanExistsErrorMessage);
+            Quantity = newQuantity;
+            CheckAndSetAvailability(Quantity);
+            return this;
+        }
+
+        private void CheckAndSetAvailability(int quantity)
+        {
+            Availability = quantity >= MinAvailableQuantity;
+        }
+
+        private void ThrowExceptionIfNameIncorrect(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(nameof(value));
+            if (value.Length < MinNameLength || value.Length > MaxNameLength) throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        private void ThrowExceptionIfPriceIncorrect(double value)
+        {
+            if (value < MinPrice || value > MaxPrice) throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        private void ThrowExceptionIfDescriptionIncorrect(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new ArgumentNullException(nameof(value));
+            if (value.Length > MaxDescriptionLength) throw new ArgumentOutOfRangeException(nameof(value));
+        }
+
+        private void ThrowExceptionIfQuantityIncorrect(int value)
+        {
+            if (value < MinQuantity || value > MaxQuantity) throw new ArgumentOutOfRangeException(nameof(value));
         }
     }
 }
---
+++ Tests/Common/WorldOfPowerTools.Domain.Test/Models/Entities/ProductTest.cs
@@ -0,0 +1,268 @@
+﻿using NUnit.Framework;
+using System;
+using WorldOfPowerTools.Domain.Enums;
+using WorldOfPowerTools.Domain.Models.Entities;
+
+namespace WorldOfPowerTools.Domain.Test.Models.Entities
+{
+    public class ProductTest
+    {
+        [Test]
+        [TestCaseSource(nameof(IncorrectConstructCases))]
+        public void CreateProductIncorrect(string name, double price, Category category, string description, int quantity, Type awaitingException)
+        {
+            TestDelegate construct = () => new Product(name, price, category, description, quantity);
+            Assert.Throws(awaitingException, construct);
+        }
+
+        [Test]
+        public void CreateProductCorrect()
+        {
+            var expProduct = CreateProduct();
+            var personData = CreateProduct();
+            Assert.True(IsEqualProducts(expProduct, personData));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(IncorrectSetNameCases))]
+        public void SetNameIncorrectValue(string value, Type awaitingException)
+        {
+            var product = CreateProduct();
+            Assert.Throws(awaitingException, () => product.Name = value);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(CorrectSetNameCases))]
+        public void SetNameСorrectValue(string value)
+        {
+            string newName = value;
+            var expProduct = CreateProduct(name: newName);
+            var product = CreateProduct();
+            product.Name = newName;
+            Assert.True(IsEqualProducts(expProduct, product));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(IncorrectSetPriceCases))]
+        public void SetPriceIncorrectValue(double value, Type awaitingException)
+        {
+            var product = CreateProduct();
+            Assert.Throws(awaitingException, () => product.Price = value);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(CorrectSetPriceCases))]
+        public void SetPriceСorrectValue(double value)
+        {
+            double newPrice = value;
+            var expProduct = CreateProduct(price: newPrice);
+            var product = CreateProduct();
+            product.Price = newPrice;
+            Assert.True(IsEqualProducts(expProduct, product));
+        }
+
+        [Test]
+        public void SetCategoryCorrectValue()
+        {
+            var newCategory = Category.Screwdriver;
+            var expProduct = CreateProduct(category: newCategory);
+            var product = CreateProduct();
+            product.Category = newCategory;
+            Assert.True(IsEqualProducts(expProduct, product));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(IncorrectSetDescriptionCases))]
+        public void SetDescriptionIncorrectValue(string value, Type awaitingException)
+        {
+            var product = CreateProduct();
+            Assert.Throws(awaitingException, () => product.Description = value);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(CorrectSetDescriptionCases))]
+        public void SetDescriptionСorrectValue(string value)
+        {
+            string newDescription = value;
+            var expProduct = CreateProduct(description: newDescription);
+            var product = CreateProduct();
+            product.Description = newDescription;
+            Assert.True(IsEqualProducts(expProduct, product));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(IncorrectAdditionCases))]
+        public void AddToStoreIncorrectValue(int value, Type awaitingException)
+        {
+            var product = CreateProduct();
+            Assert.Throws(awaitingException, () => product.AddToStore(value));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(AddMoreFromStoreThanMaxQuantityCases))]
+        public void AddMoreFromStoreThanMaxQuantity(int oldQuantity, int addition, Type awaitingException)
+        {
+            var product = CreateProduct(quantity: oldQuantity);
+            Assert.Throws(awaitingException, () => product.AddToStore(addition));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(CorrectAdditionCases))]
+        public void AddToStoreCorrectValue(int oldQuantity, int addition, int newQuantity)
+        {
+            var expProduct = CreateProduct(quantity: newQuantity);
+            var product = CreateProduct(quantity: oldQuantity);
+            product.AddToStore(addition);
+            Assert.True(IsEqualProducts(expProduct, product));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(IncorrectRemovalCases))]
+        public void RemoveFromStoreIncorrectValue(int value, Type awaitingException)
+        {
+            var product = CreateProduct();
+            Assert.Throws(awaitingException, () => product.RemoveFromStore(value));
+        }
+
+        [Test]
+        [TestCase(100, 101, typeof(InvalidOperationException))]
+        public void RemoveMoreFromStoreThanExists(int oldQuantity, int removal, Type awaitingException)
+        {
+            var product = CreateProduct(quantity: oldQuantity);
+            Assert.Throws(awaitingException, () => product.RemoveFromStore(removal));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(CorrectRemovalCases))]
+        public void RemoveFromStoreCorrectValue(int oldQuantity, int removal, int newQuantity)
+        {
+            var expProduct = CreateProduct(quantity: newQuantity);
+            var product = CreateProduct(quantity: oldQuantity);
+            product.RemoveFromStore(removal);
+            Assert.True(IsEqualProducts(expProduct, product));
+        }
+
+        [Test]
+        public void AvailabilityChangeAfterProductEnded()
+        {
+            var expProduct = CreateProduct(quantity: Product.MinQuantity, isAvailable : false);
+            var product = CreateProduct(quantity: 100);
+            product.RemoveFromStore(100);
+            Assert.True(IsEqualProducts(expProduct, product));
+        }
+
+        [Test]
+        public void AvailabilityChangeAfterProductAppeared()
+        {
+            var expProduct = CreateProduct(quantity: Product.MinQuantity + 150, isAvailable: true);
+            var product = CreateProduct(quantity: Product.MinQuantity);
+            product.AddToStore(150);
+            Assert.True(IsEqualProducts(expProduct, product));
+        }
+
+        private Product CreateProduct(string name = "Тестовый товар", string description = "Тестовое описание",
+            double price = 150d, Category category = Category.Painting, int quantity = 100, bool isAvailable = true)
+        {
+            return new Product(name, price, category, description, quantity, isAvailable);
+        }
+
+        private bool IsEqualProducts(Product a, Product b)
+        {
+            return a.Name == b.Name &&
+                a.Price == b.Price &&
+                a.Category == b.Category &&
+                a.Description == b.Description &&
+                a.Price == b.Price &&
+                a.Quantity == b.Quantity &&
+                a.Availability == b.Availability;
+        }
+
+        static readonly object[] IncorrectConstructCases =
+        {
+            new object?[] { "", 150d, Category.Painting, "Тестовое описание", 100, typeof(ArgumentNullException) },
+            new object?[] { null, 150d, Category.Painting, "Тестовое описание", 100, typeof(ArgumentNullException) },
+            new object?[] { "".PadLeft(Product.MinNameLength - 1), 150d, Category.Painting, "Тестовое описание", 100, typeof(ArgumentOutOfRangeException) },
+            new object?[] { "".PadLeft(Product.MaxNameLength + 1), 150d, Category.Painting, "Тестовое описание", 100, typeof(ArgumentOutOfRangeException) },
+            new object?[] { "Тестовый товар", Product.MinPrice - 1, Category.Painting, "Тестовое описание", 100, typeof(ArgumentOutOfRangeException) },
+            new object?[] { "Тестовый товар", Product.MaxPrice + 1, Category.Painting, "Тестовое описание", 100, typeof(ArgumentOutOfRangeException) },
+            new object?[] { "Тестовый товар", 150d, Category.Painting, "", 100, typeof(ArgumentNullException) },
+            new object?[] { "Тестовый товар", 150d, Category.Painting, null, 100, typeof(ArgumentNullException) },
+            new object?[] { "Тестовый товар", 150d, Category.Painting, "".PadLeft(Product.MaxDescriptionLength + 1, '-'), 100, typeof(ArgumentOutOfRangeException) },
+            new object?[] { "Тестовый товар", 150d, Category.Painting, "Тестовое описание", Product.MinQuantity - 1, typeof(ArgumentOutOfRangeException) },
+            new object?[] { "Тестовый товар", 150d, Category.Painting, "Тестовое описание", Product.MaxQuantity + 1, typeof(ArgumentOutOfRangeException) },
+        };
+
+        static readonly object[] IncorrectSetNameCases =
+        {
+            new object?[] { "", typeof(ArgumentNullException) },
+            new object?[] { null, typeof(ArgumentNullException) },
+            new object?[] { "".PadLeft(Product.MinNameLength - 1), typeof(ArgumentOutOfRangeException) },
+            new object?[] { "".PadLeft(Product.MaxNameLength + 1), typeof(ArgumentOutOfRangeException) }
+        };
+
+        static readonly object[] CorrectSetNameCases =
+{
+            "".PadLeft(Product.MinNameLength),
+            "".PadLeft(Product.MinNameLength + 1),
+            "".PadLeft(Product.MaxNameLength),
+            "".PadLeft(Product.MaxNameLength - 1)
+        };
+
+        static readonly object[] IncorrectSetPriceCases =
+{
+            new object?[] { -Product.MaxPrice, typeof(ArgumentOutOfRangeException) },
+            new object?[] { Product.MinPrice - 1, typeof(ArgumentOutOfRangeException) },
+            new object?[] { Product.MaxPrice + 1, typeof(ArgumentOutOfRangeException) },
+        };
+
+        static readonly object[] CorrectSetPriceCases =
+        {
+            Product.MinPrice,
+            Product.MaxPrice,
+        };
+
+        static readonly object[] IncorrectSetDescriptionCases =
+{
+            new object?[] { "", typeof(ArgumentNullException) },
+            new object?[] { null, typeof(ArgumentNullException) },
+            new object?[] { "".PadLeft(Product.MaxDescriptionLength + 1), typeof(ArgumentOutOfRangeException) },
+        };
+
+        static readonly object[] CorrectSetDescriptionCases =
+        {
+            "d",
+            "".PadLeft(Product.MaxDescriptionLength - 1),
+        };
+
+        static readonly object[] IncorrectAdditionCases =
+        {
+            new object?[] { -Product.MaxQuantity, typeof(ArgumentOutOfRangeException) },
+            new object?[] { -1, typeof(ArgumentOutOfRangeException) },
+            new object?[] { Product.MaxQuantity + 1, typeof(InvalidOperationException) },
+        };
+
+        static readonly object[] CorrectAdditionCases =
+        {
+            new object[] { Product.MinQuantity, 100, 100 + Product.MinQuantity },
+            new object[] { Product.MinQuantity, Product.MaxQuantity - Product.MinQuantity, Product.MaxQuantity, },
+        };
+
+        static readonly object[] AddMoreFromStoreThanMaxQuantityCases =
+        {
+            new object[] { 100, Product.MaxQuantity, typeof(InvalidOperationException) }
+        };
+
+        static readonly object[] IncorrectRemovalCases =
+{
+            new object?[] { -Product.MaxQuantity, typeof(ArgumentOutOfRangeException) },
+            new object?[] { -1, typeof(ArgumentOutOfRangeException) },
+            new object?[] { Product.MaxQuantity + 1, typeof(InvalidOperationException) },
+        };
+
+        static readonly object[] CorrectRemovalCases =
+        {
+            new object[] { Product.MinQuantity + 101, 100, Product.MinQuantity + 1 },
+            new object[] { Product.MaxQuantity, Product.MaxQuantity - Product.MinQuantity - 1, Product.MinQuantity + 1, },
+        };
+    }
+}
