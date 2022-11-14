214f15c7d1f416eb65ba5311cc4a039a4ed938d8
Nike073i <nike073i@mail.ru>
2022-11-14 17:51:10 +0400

    Реализация DbProductRepository

--- Common/WorldOfPowerTools.Domain/Repositories/IProductRepository.cs
+++ Common/WorldOfPowerTools.Domain/Repositories/IProductRepository.cs
@@ -5,7 +5,7 @@ namespace WorldOfPowerTools.Domain.Repositories
 {
     public interface IProductRepository : IRepository<Product>
     {
-        public Task<IEnumerable<Product>> GetByCategoryAsync(Category category, int? skip, int? take);
+        public Task<IEnumerable<Product>> GetByCategoryAsync(Category category, int skip = 0, int? take = null);
         public Task<IEnumerable<Product>> SaveRangeAsync(IEnumerable<Product> products);
     }
 }
--- Data/WorldOfPowerTools.DAL/Repositories/DbProductRepository.cs
+++ Data/WorldOfPowerTools.DAL/Repositories/DbProductRepository.cs
@@ -1,3 +1,4 @@
+using Microsoft.EntityFrameworkCore;
 using WorldOfPowerTools.DAL.Context;
 using WorldOfPowerTools.Domain.Enums;
 using WorldOfPowerTools.Domain.Models.Entities;
@@ -9,14 +10,34 @@ namespace WorldOfPowerTools.DAL.Repositories
     {
         public DbProductRepository(WorldOfPowerToolsDb context) : base(context) { }
 
-        public Task<IEnumerable<Product>> GetByCategoryAsync(Category category, int? skip, int? take)
+        public async Task<IEnumerable<Product>> GetByCategoryAsync(Category category, int skip = 0, int? take = null)
         {
-            throw new NotImplementedException();
+            var itemsByCategories = Items.Where(product => product.Category == category);
+            int skipCount = skip < 0 ? 0 : skip;
+            var items = itemsByCategories.Skip(skipCount);
+            if (take.HasValue && take.Value > 0)
+                items = items.Take(take.Value);
+            return await items.ToListAsync();
         }
 
-        public Task<IEnumerable<Product>> SaveRangeAsync(IEnumerable<Product> products)
+        public async Task<IEnumerable<Product>> SaveRangeAsync(IEnumerable<Product> products)
         {
-            throw new NotImplementedException();
+            using var transaction = DbContext.Database.BeginTransaction();
+            {
+                try
+                {
+                    foreach (var product in products)
+                        await SaveAsync(product);
+                    DbContext.SaveChanges();
+                    transaction.Commit();
+                    return products;
+                }
+                catch
+                {
+                    transaction.Rollback();
+                    return Enumerable.Empty<Product>();
+                }
+            }
         }
     }
 }
